9a36f8d8a6c4d35044b51041144667fdbe4415d1
Vladeta-Milosavljevic <vertigo.vm@outlook.com>
2023-11-14 22:08:43 +0100

    Pagination changes

--- proba/jobs/helpers.py
+++ proba/jobs/helpers.py
@@ -25,51 +25,27 @@ def send_application(request, form):
 
 def pagination(request, jobs):
     page = request.GET.get('page', 1)
-    
+
     # izbacivanje netacnih podataka
     try:
         items_per_page = int(request.GET.get('items_per_page', 2))
+        items_per_page = 2 if items_per_page == 0 or items_per_page > 6 else items_per_page
     except:
         items_per_page = 2
-      
-    if items_per_page is 0 | items_per_page >6:
-            items_per_page = 2 
-             
-    paginator = Paginator(jobs, items_per_page)
 
+    paginator = Paginator(jobs, items_per_page)
+    
+    try:
+        page = int(request.GET.get('page', 1))
+        page = 1 if page==0 or page > paginator.num_pages else page    
+    except:
+        page=1
     try:
         page_obj = paginator.page(page)
     except InvalidPage:
         page_obj = paginator.page(1)
-        page=1
-
-    # # ako se rucno ubaci string
-    # try:
-    #     items_per_page = int(items_per_page)
-    # except:
-    #     items_per_page = 2
-
-    # # ako se rucno ubaci preveliki broj poslova po stranici
-    # if items_per_page > 6 or items_per_page == 0:
-    #     items_per_page = 2
-    # paginator = Paginator(jobs, items_per_page)
-    # page = request.GET.get('page', 1)
-
-    # # ako se rucno ubaci string
-    # try:
-    #     page = int(page)
-    # except:
-    #     page = 1
-
-    # # ako se rucno ubaci nepostojeca stranica
-    # if len(jobs) % 2 == 0:
-    #     if page > int(len(jobs)/items_per_page) or page == 0:
-    #         page = 1
-    # else:
-    #     if page > int(len(jobs)/items_per_page)+1 or page == 0:
-    #         page = 1
-
-    # page_obj = paginator.get_page(page)
+        page = 1
+   
     page_range = paginator.get_elided_page_range(
         number=page, on_each_side=1, on_ends=1)
 
